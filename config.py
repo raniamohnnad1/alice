c3391882df9ce12fc0cb8563808b7eff578c2fd2
raniamohnnad1 <raniamohnnad@gmail.com>
2025-08-15 04:58:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 12134731
+API_HASH = "492580172928369fab557e4213c8734f"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "8403549313:AAH2o0q7lSuHrvN2xCEV9DNrnb9hhuV5Zao"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://Rania123344:@cluster0.fmrbrws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002712351515
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 7549694299
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/v4aav"
+SUPPORT_GROUP = "https://t.me/HFETT"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "1AZWarzIAUBDEeUontv5zrI6H183JLm3X6n_ToQw_UTNAt2YZEkEy0jrYx7ciZvnQaLx8udcaW1f2k4x6f4mxXxAOeX9USwRQEASK_qdOfHT3Z7ixnU0aTOX7KjS2YsvVqbqedw6GbE1uPT2e7hkbCbPxaCcXhQlf_LuSDY0yH5ATFhSAY-mxFNF7Z1uq2vddQUUikrHbRwY2XwPyKZ6bpqBfJDhZuRIHtVW0SEfMrLcbJ8ZNU4Q8D1f6JFQgXaV7zwz5E-C_FY6VA_ROyaoG15CC_Hn62MkDwu3DJTIVf-LgRuG3s1BXXPQkQTqJSKeWXkgzZMhiZEKPo9Pimr65du_xTQmEtLs="
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -77,9 +77,9 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://graph.org/file/cd8cfb5187fdffa86975b-120b42c3737d1609ed.jpg"
 
-PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://graph.org/file/cd8cfb5187fdffa86975b-120b42c3737d1609ed.jpg"
 
 PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
 STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
@@ -112,3 +112,4 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
